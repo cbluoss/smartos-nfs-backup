81a56dd0fade1f791b8b8b0e785484f823ce2acf
yourcoke <yourcoke@users.noreply.github.com>
2016-04-25 22:31:51 +0200

    Create nfs-backup.sh

---
+++ nfs-backup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+# Backup all (kvm) virtual machines (disks+json data) on a SmartOS hypervisor
+# Intended for nfs/remote backups (if you want to keep them locally, stick to zfs snapshots)
+# Original Author:  russell@ballestrini.net http://russell.ballestrini.net/
+# Author: Yourcoke 
+# E-Mail: deinkoks@gmail.com
+# This Version does not use vmadm send but use zfs send instead
+
+# Backup directory without trailing slash
+backupdir=/net/HOSTNAMEorIP/backups/smartos/vms
+
+tmpdir=/opt/tmp
+
+svcadm enable autofs
+
+for VM in `vmadm list -p -o alias,uuid`
+  do
+    # create an array called VM_PARTS splitting on ':'
+    IFS=':' VM_PARTS=($VM)
+
+    # create some helper varibles for alias and uuid
+    alias=${VM_PARTS[0]}
+    uuid=${VM_PARTS[1]}
+    #zfs snapshot 
+    echo "Backup started for $VM"
+    vmadm get $uuid > $backupdir/$alias-$uuid.json
+    #vmadm send $uuid > $tmpdir/$alias
+    IFS=$'\n'
+    for disk in `vmadm get ${VM_PARTS[1]}| json disks | grep zfs_filesystem | awk -F \" '{printf "%s@backup\n", $4}'`
+    do
+        if [[ $disk == *@* ]]
+        then
+            zfs snapshot $disk
+            zfs send $disk > $backupdir/$alias-$uuid.zfs
+            zfs destroy -r $disk
+        fi
+    done
+
+  done
+
+svcadm disable autofs
